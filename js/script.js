54f4cded0558ac90fd7893141ca9ee17e59b642e
amreshkrmahato <amreshkrmahato1123@gmail.com>
2026-06-02 05:09:34 +0530

    update resume and profile picture, add logo, and make minor adjustments to the website

--- js/script.js
+++ js/script.js
@@ -24,10 +24,36 @@ $(document).ready(function(){
       loop: true
     });
 
-    //toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
+    // toggle menu/navbar script (improved): switch icon and close on link click
+    $('.menu-btn').on('click keypress', function(e){
+        if(e.type === 'keydown' && !(e.key === 'Enter' || e.key === ' ' || e.key === 'Spacebar')) return;
+      var menu = $('.navbar .menu');
+      menu.toggleClass('active');
+      $('body').toggleClass('no-scroll');
+      var icon = $('.menu-btn i');
+      var expanded = $(this).attr('aria-expanded') === 'true';
+      $(this).attr('aria-expanded', !expanded);
+      menu.attr('aria-hidden', expanded);
+      // toggle between bars and times (x) icon
+      if(icon.hasClass('fa-bars')){
+        icon.removeClass('fa-bars').addClass('fa-times');
+      } else {
+        icon.removeClass('fa-times').addClass('fa-bars');
+      }
+    });
+
+    // close mobile menu when a nav link is clicked
+    $('.navbar .menu li a').click(function(){
+      var menu = $('.navbar .menu');
+      if(menu.hasClass('active')){
+            menu.removeClass('active');
+            $('body').removeClass('no-scroll');
+        var btn = $('.menu-btn');
+        btn.attr('aria-expanded', 'false');
+        menu.attr('aria-hidden', 'true');
+        var icon = $('.menu-btn i');
+        icon.removeClass('fa-times').addClass('fa-bars');
+      }
     });
     //owl carousel script
     $('.carousel').owlCarousel({
